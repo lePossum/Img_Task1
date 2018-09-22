37786bdb98b722f8f03be444e8492e5a7357cd06
lePossum <38914945+lePossum@users.noreply.github.com>
2018-09-22 20:38:28 +0300

    Start upload

---
+++ Img_Task1/bin/AnyCPU/Debug/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+
+namespace ImageReadCS
+{
+    class Program
+    {
+        static void FlipImage(GrayscaleFloatImage image)
+        {
+            for (int y = 0; y < image.Height; y++)
+                for (int x = 0; x < image.Width / 2; x++)
+                {
+                    float p = image[x, y];
+                    image[x, y] = image[image.Width - 1 - x, y];
+                    image[image.Width - 1 - x, y] = p;
+                }
+        }
+
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 2) {
+                Console.WriteLine("No input files12345");
+                return;
+            }
+            string InputFileName = args[0], OutputFileName = args[1];
+            if (!File.Exists(InputFileName))
+            {
+                Console.WriteLine("No Input file");
+                return;
+            }
+            GrayscaleFloatImage image = ImageIO.FileToGrayscaleFloatImage(InputFileName);
+
+            FlipImage(image);
+
+            ImageIO.ImageToFile(image, OutputFileName);
+
+            Console.ReadKey();
+        }
+    }
+}
